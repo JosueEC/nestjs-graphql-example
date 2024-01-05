07bc3fef111c6dd16c558d1918cb3f0d45c0c698
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-04 18:37:50 -0600

    feat(main.ts): 🔨 Activate the global pipes in the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * Esta funcion permite activar las validaciones definidas con
+   * class-validator y class-transformer en todos los endpoints de
+   * nuestra API
+   */
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
